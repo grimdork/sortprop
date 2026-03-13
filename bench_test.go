36ac5965daf3812844035d50fb92dbcf96f8b2e7
Urethramancer <github@grimdork.net>
2026-03-13 04:25:22 +0100

    Add uniqueness-ratio sweep to benches for weird cases

--- bench_test.go
+++ bench_test.go
@@ -40,69 +40,73 @@ func BenchmarkUniqueKeys_All(b *testing.B) {
 						_ = UniqueKeys(kp, false)
 					}
 				})
-			b.Run("Map_first", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueKeysMap(kp, false)
-				}
+				b.Run("Map_first", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueKeysMap(kp, false)
+					}
+				})
+				b.Run("Hybrid_first", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueKeysHybrid(kp, false)
+					}
+				})
+				b.Run("Sort_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueKeys(kp, true)
+					}
+				})
+				b.Run("Map_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueKeysMap(kp, true)
+					}
+				})
+				b.Run("Hybrid_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueKeysHybrid(kp, true)
+					}
+				})
 			})
-			b.Run("Hybrid_first", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueKeysHybrid(kp, false)
-				}
-			})
-			b.Run("Sort_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueKeys(kp, true)
-				}
-			})
-			b.Run("Map_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueKeysMap(kp, true)
-				}
-			})
-			b.Run("Hybrid_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueKeysHybrid(kp, true)
-				}
-			})
-		})
+		}
 	}
 }
 
 func BenchmarkUniqueValues_All(b *testing.B) {
 	for _, n := range []int{1000, 10000} {
-		b.Run(fmt.Sprintf("n=%d", n), func(b *testing.B) {
-			vp := genVP(n, ur)
-			b.Run("Sort_first", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValues(vp, false)
-				}
+		for _, ur := range []float64{0.01, 0.1, 0.5, 1.0} {
+			label := fmt.Sprintf("n=%d/ur=%.2f", n, ur)
+			b.Run(label, func(b *testing.B) {
+				vp := genVP(n, ur)
+				b.Run("Sort_first", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValues(vp, false)
+					}
+				})
+				b.Run("Map_first", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValuesMap(vp, false)
+					}
+				})
+				b.Run("Hybrid_first", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValuesHybrid(vp, false)
+					}
+				})
+				b.Run("Sort_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValues(vp, true)
+					}
+				})
+				b.Run("Map_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValuesMap(vp, true)
+					}
+				})
+				b.Run("Hybrid_last", func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_ = UniqueValuesHybrid(vp, true)
+					}
+				})
 			})
-			b.Run("Map_first", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValuesMap(vp, false)
-				}
-			})
-			b.Run("Hybrid_first", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValuesHybrid(vp, false)
-				}
-			})
-			b.Run("Sort_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValues(vp, true)
-				}
-			})
-			b.Run("Map_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValuesMap(vp, true)
-				}
-			})
-			b.Run("Hybrid_last", func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					_ = UniqueValuesHybrid(vp, true)
-				}
-			})
-		})
+		}
 	}
 }
